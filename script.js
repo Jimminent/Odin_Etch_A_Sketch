b0c38bec78d7b4ad4434b8c52c23aeb0d3b82729
James ORourke <jimao137@gmail.com>
2022-08-16 17:27:23 -0400

    create functions to delete and create the different sketchboards

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-let side = 16
+let side = 16;
 let grid = side * side;
+let newSide;
+let container = document.getElementById("gridContainer");
 
-for (i = 0; i < grid; i++) {
-    let item = document.createElement("div");
-    let element = document.getElementById("gridContainer");
-    element.appendChild(item);
-    item.addEventListener("mouseover", (event) => {
-        item.style.backgroundColor = "black";
-    });
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
+
+function setup(input) {
+    for (i = 0; i < input; i++) {
+        let item = document.createElement("div");
+        container.appendChild(item);
+        item.addEventListener("mouseover", (event) => {
+            item.style.backgroundColor = "black";
+        });
+    }
+}
+
+setup(grid);
+
+let button = document.getElementById("reset");
+button.addEventListener("click", (event) => {
+    newSide = prompt("how many squares to a side?");
+    removeAllChildNodes(container);
+    grid = newSide * newSide;
+    setup(grid);
+
+});
 
